54650e13d961d3860197e5f5928ee326c80bbb36
Szymon Łukaszuk <bezimienny.bezi@o2.pl>
2022-06-13 13:34:29 +0200

    Zrobione

---
+++ main.js
@@ -0,0 +1,15 @@
+function funkcja(liczba1, liczba2) {
+    if (liczba1 = 20 ) {
+        return true;
+    }
+
+    else if (liczba2 = 20){
+        return true;
+    }
+
+    else if ((liczba1 + liczba2) <= 20) {
+        return true;
+    }
+}
+
+console.log=(funkcja(2, 12));
